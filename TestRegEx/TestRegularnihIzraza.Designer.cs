632ed2f9851ab49c984d9847137985106d1f528e
Sara Cuzele <saracuzele@gmail.com>
2020-02-24 15:07:13 +0100

    Added controls for testing regular expressions

---
+++ TestRegEx/TestRegularnihIzraza.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace TestRegEx
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Unesite tekst";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(55, 108);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(594, 470);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(55, 662);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(594, 31);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(50, 616);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(227, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Unesite regularni izraz";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(50, 718);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(488, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Unesite tekst kojim zelite zamijeniti regularni izraz";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(55, 760);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(594, 31);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(805, 108);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(586, 683);
+            this.textBox4.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(800, 54);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Rezultat";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1485, 885);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Test Regularnih Izraza";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ TestRegEx/TestRegularnihIzraza.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace TestRegEx
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(textBox3.Text))
+            {
+                Search();
+            }
+            else
+            {
+                Replace();
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(textBox3.Text))
+            {
+                Search();
+            }
+            else
+            {
+                Replace();
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(textBox3.Text))
+            {
+                Search();
+            }
+            else
+            {
+                Replace();
+            }
+        }
+
+        private void Search()
+        {
+            var text = textBox1.Text;
+            var stringRegex = textBox2.Text;
+            var regex = new Regex(stringRegex);
+            var result = "";
+
+            foreach (var item in regex.Matches(text))
+            {
+                result += item;
+                result += "\n";
+            }
+
+            textBox4.Text = result;
+        }
+
+        private void Replace()
+        {
+            var text = textBox1.Text;
+            var replacement = textBox3.Text;
+            var stringRegex = textBox2.Text;
+            var regex = new Regex(stringRegex);
+            var result = regex.Replace(text, replacement);
+
+            textBox4.Text = result;
+        }
+    }
+}
